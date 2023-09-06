6b8a5d024304713eec6ebae139df46f678ee911a
unknown <goldenderrick95@gmail.com>
2023-09-06 23:09:53 +0300

    express params

--- app.js
+++ app.js
@@ -1,14 +1,32 @@
 
 const express = require('express')
 const app = express()
+const { products } = require('./data')
 
 const path = require('path')
 
 // app.use(express.static('navbar-app'))
 
-app.get('/', (req,res) =>{
+app.get('/products/:id', (req,res) =>{
+    const {id} = req.params
+    const prod = products.find((product) => product.id === Number(id))
     // res.sendFile(path.resolve(__dirname,'navbar-app','index.html'))
-    res.json([{name:"Derrick"}])
+    res.json(prod)
+})
+
+app.get('/api/v1/query/?', (req,res) =>{
+    console.log(req.query)
+    const {search, limit} = req.query
+
+    let sortedProducts = [...products]
+    if(search){
+        sortedProducts = products.filter(product => product.name.startsWith(search))
+    }
+    if(limit){
+        sortedProducts = sortedProducts.slice(0, Number(limit))
+    }
+
+    res.status(200).send(sortedProducts)
 })
 
 app.all('*',(req, res)=>{
